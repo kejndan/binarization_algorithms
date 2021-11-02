49f65495ddb60e04d364b958cc24aa84c6d0f01d
Adel <shi.samadel@gmail.com>
2021-11-02 20:05:21 +0300

    add meta class

---
+++ core/binarization.py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+
+class Binarization:
+    def __init__(self):
+        pass
+
+    def read_img(self, path):
+        self.img =  cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
+        return self.img
+
+    def save_img(self, path):
+        cv2.imwrite(path, self.img)
+
+    def show_img(self):
+        plt.imshow(self.img)
+        plt.axis(False)
+        plt.show()
+
+
