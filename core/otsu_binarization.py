972e186f8293285817ce8fff7a907230bbcf8a96
Adel <shi.samadel@gmail.com>
2021-11-02 20:24:20 +0300

    bug fix with indexes

--- core/otsu_binarization.py
+++ core/otsu_binarization.py
@@ -13,14 +13,14 @@ class OtsuBinarization(Binarization):
         thresholds = np.zeros(256)
 
         for k in range(256):
-            iters_0 = np.arange(0, k)
-            iters_1 = np.arange(k, 256)
+            iters_0 = np.arange(0, k + 1)
+            iters_1 = np.arange(k + 1, 256)
 
-            w_0 = histogram[:k].sum()/N
+            w_0 = histogram[:k + 1].sum()/N
             w_1 = 1 - w_0
 
-            m_0 = (histogram[:k]*iters_0).sum()/w_0 if w_0 != 0 else 0
-            m_1 = (histogram[k:]*iters_1).sum()/w_1 if w_1 != 0 else 0
+            m_0 = (histogram[:k + 1]*iters_0).sum()/w_0 if w_0 != 0 else 0
+            m_1 = (histogram[k + 1:]*iters_1).sum()/w_1 if w_1 != 0 else 0
 
             t = w_0*w_1*(m_0 - m_1)**2
             thresholds[k] = t
